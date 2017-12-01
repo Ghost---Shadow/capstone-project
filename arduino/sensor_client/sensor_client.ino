efdb1de1b437329958292920d704eee88de74645
Souradeep Nanda <souradeep.nanda2014@vit.ac.in>
2017-12-01 09:46:34 +0530

    Board Networking

---
+++ arduino/sensor_client/sensor_client.ino
@@ -0,0 +1,131 @@
+#include <SPI.h>
+#include <WiFi.h>
+
+#define PIN_THERMOMETER 1
+#define PIN_MOISTURE 2
+#define PIN_LDR 3
+
+#define CALIBRATION_THERMOMETER 1.0f
+#define CALIBRATION_MOISTURE 1.0f
+#define CALIBRATION_LDR 1.0f
+
+#define PIN_FAN 6
+#define PIN_PUMP 7
+#define PIN_LAMP 8
+
+#define PORT 8080
+#define SERVER String("127.0.0.1")
+#define INTERVAL 1000
+
+char ssid[] = "NetworkName"; 
+char pass[] = "NetPassword123";    
+int keyIndex = 0;    
+int lastConnectionTime = 0;
+
+int status = WL_IDLE_STATUS;
+
+WiFiClient client;
+
+void setup() {
+  Serial.begin(9600);
+  
+  while (!Serial);    
+ 
+  if (WiFi.status() == WL_NO_SHIELD) {
+    Serial.println("WiFi shield not present");
+    // don't continue:
+    while(true);
+  }
+ 
+  // attempt to connect to Wifi network:
+  while (status != WL_CONNECTED) {
+    Serial.print("Attempting to connect to SSID: ");
+    Serial.println(ssid);
+    // Connect to WPA/WPA2 network. Change this line if using open or WEP network:    
+    status = WiFi.begin(ssid, pass);
+ 
+    // wait 5 seconds for connection:
+    delay(5000);
+  }
+  Serial.println("Connected to wifi");
+  printWifiStatus(); 
+}
+
+
+void loop() {
+  String request = "";
+  while (client.available()) {
+    char c = client.read();
+    request += c;
+  }
+  if(request.length() > 0){
+    Serial.println("Incomming: "+request);
+    parseAndServiceRequest(request);
+  }  
+
+  if (millis() - lastConnectionTime > INTERVAL) {
+    String data = stringifyData();
+    Serial.println("Outgoing: "+data);
+    httpRequest(data);
+  }
+  delay(100);
+}
+
+void httpRequest(String data) {
+  // close any connection before send a new request.
+  // This will free the socket on the WiFi shield
+  client.stop();
+
+  // if there's a successful connection:
+  if (client.connect(SERVER.c_str(), PORT)) {
+    Serial.println("connecting...");
+    // send the HTTP PUT request:
+    client.println("GET "+data+" HTTP/1.1");
+    client.println("Host: "+SERVER);
+    client.println("User-Agent: ArduinoWiFi/1.1");
+    client.println("Connection: close");
+    client.println();
+
+    // note the time that the connection was made:
+    lastConnectionTime = millis();
+  } else {
+    // if you couldn't make a connection:
+    Serial.println("connection failed");
+  }
+}
+
+// Format /upload?t=1023&m=1023&l=1023
+String stringifyData(){
+  String allData = "";
+  int t = analogRead(PIN_THERMOMETER);
+  int m = analogRead(PIN_MOISTURE);
+  int l = analogRead(PIN_LDR);
+  allData = "/upload?t="+String(t)+"&m="+String(m)+"&l="+String(l);
+  return allData;
+}
+
+// Expected format ?m=101
+void parseAndServiceRequest(String request){
+  int r = request.substring(3).toInt();
+  
+  digitalWrite(PIN_FAN ,r & (1<<0));
+  digitalWrite(PIN_PUMP,r & (1<<1));
+  digitalWrite(PIN_LAMP,r & (1<<2));
+}
+
+void printWifiStatus() {
+  // print the SSID of the network you're attached to:
+  Serial.print("SSID: ");
+  Serial.println(WiFi.SSID());
+
+  // print your WiFi shield's IP address:
+  IPAddress ip = WiFi.localIP();
+  Serial.print("IP Address: ");
+  Serial.println(ip);
+
+  // print the received signal strength:
+  long rssi = WiFi.RSSI();
+  Serial.print("signal strength (RSSI):");
+  Serial.print(rssi);
+  Serial.println(" dBm");
+}
--- arduino/sensor_testing/sensor_testing.ino
+++ arduino/sensor_testing/sensor_testing.ino
@@ -2,21 +2,17 @@
 #define PIN_MOISTURE 2
 #define PIN_LDR 3
 
-#define CALIBRATION_THERMOMETER 1.0f
-#define CALIBRATION_MOISTURE 1.0f
-#define CALIBRATION_LDR 1.0f
-
 #define PIN_FAN 6
-#define PIN_WATER_PUMP 7
+#define PIN_PUMP 7
 #define PIN_LAMP 8
 
-float val;
+int val;
 String out;
 
 void setup(){
   Serial.begin(9600);
   pinMode(PIN_FAN,OUTPUT);
-  pinMode(PIN_WATER_PUMP,OUTPUT);
+  pinMode(PIN_PUMP,OUTPUT);
   pinMode(PIN_LAMP,OUTPUT);
   
   pinMode(13, OUTPUT); // debug
@@ -31,18 +27,18 @@ void toggleWithDelay(int pin,int delayLength){
 
 void loop(){
   digitalWrite(13, HIGH);
-  val = analogRead(PIN_THERMOMETER) / CALIBRATION_THERMOMETER;
+  val = analogRead(PIN_THERMOMETER);
   Serial.println("Temperature: "+String(val));
   
-  val = analogRead(PIN_MOISTURE) / CALIBRATION_MOISTURE;  
+  val = analogRead(PIN_MOISTURE);  
   Serial.println("Moisture: "+String(val));  
   
-  val = analogRead(PIN_LDR) / CALIBRATION_LDR;  
+  val = analogRead(PIN_LDR);  
   Serial.println("Light: "+String(val)); 
   digitalWrite(13, LOW);
 
   toggleWithDelay(PIN_FAN,3000); 
-  toggleWithDelay(PIN_WATER_PUMP,3000); 
+  toggleWithDelay(PIN_PUMP,3000); 
   toggleWithDelay(PIN_LAMP,1000);  
 }
 
