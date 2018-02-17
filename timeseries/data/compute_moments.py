e4c27221e43160e3a82455e5566856dff966c9fa
Souradeep Nanda <souradeep.nanda123@gmail.com>
2018-02-17 19:12:59 +0530

    changed dataset format

--- timeseries/data/compute_moments.py
+++ timeseries/data/compute_moments.py
@@ -2,27 +2,23 @@ import json
 
 import numpy as np
 
-PATH_FORMAT = './%s_values.npy'
+DATA_FILE_PATH = './dataset.npy'
 OUTPUT_FILE_NAME = './moments.json'
 
-datasets = [
-  'light',
-  'temperature',
-]
+dataset = np.load(DATA_FILE_PATH)
 
-momentDict = {
-  'light': {},
-  'temperature' : {}
-}
+moments = []
 
-for dataset in datasets:
-  data = np.load(PATH_FORMAT % dataset)
+for data in dataset:
   data_mean = float(data.mean())
   data_std = float(data.std())
   print('Moments:',data_mean, data_std)
 
-  momentDict[dataset]['mean'] = data_mean
-  momentDict[dataset]['std'] = data_std
+  momentDict = {
+      'mean':data_mean,
+      'std':data_std
+    }
+  moments.append(momentDict)
 
 with open(OUTPUT_FILE_NAME, 'w') as f:
-  json.dump(momentDict,f)
+  json.dump(moments,f)
--- timeseries/data/preprocess_data.py
+++ timeseries/data/preprocess_data.py
@@ -8,6 +8,7 @@ analog_gen = np.poly1d(np.polyfit([0,12,24],[700,300,200],2))
 temperature_gen = np.poly1d(np.polyfit([0,12,24],[20,40,20],2))
 
 light_values = [None for _ in range(DATA_SIZE)]
+moisture_values = [None for _ in range(DATA_SIZE)]
 temperature_values = [None for _ in range(DATA_SIZE)]
 
 for i in range(DATA_SIZE):
@@ -15,17 +16,25 @@ for i in range(DATA_SIZE):
     light_values[i] += np.random.rand(READINGS) * 25
     ##plt.plot(light_values[i])
     
+    moisture_values[i] = np.array([analog_gen(i) for i in np.linspace(0,24,READINGS)])
+    moisture_values[i] += np.random.rand(READINGS) * 50
+    ##plt.plot(moisture_values[i])
+    
     temperature_values[i] = [temperature_gen(i) for i in np.linspace(0,24,READINGS)]
     temperature_values[i] += np.random.rand(READINGS) * 2
     ##plt.plot(temperature_values)
 
 light_values = np.array(light_values)
+moisture_values = np.array(moisture_values)
 temperature_values = np.array(temperature_values)
 
-print(light_values.shape, temperature_values.shape)
+print(light_values.shape, moisture_values.shape, temperature_values.shape)
 
-np.save('./light_values.npy', light_values)
-np.save('./temperature_values.npy', temperature_values)
+dataset = np.array([light_values, moisture_values, temperature_values])
+
+print(dataset.shape)
+
+np.save('./dataset.npy', dataset)
 
 ##plt.show()
 
